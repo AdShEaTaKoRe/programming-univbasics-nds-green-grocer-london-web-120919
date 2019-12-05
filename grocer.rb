a98b3d11299d35d1f08ec5dde9a6de05c469a4dd
Tommy Berman <tomy.berman1@gmail.com>
2019-12-05 15:02:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,15 +28,14 @@ def consolidate_cart(cart)
   while cart_index < cart.length do
       item_name = cart[cart_index][:item]
       item_stats = cart[cart_index[:count]
-      
+      cart_index += 1
+  end
   #   if 
   #     item_stats += 1
   #     else
   #     unique_items[item_stats] = 1
   #   end
-  
-    end
-    cart_index += 1
+    
   unique_items
   binding.pry
 end
@@ -57,14 +56,14 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
 end
 
-# def checkout(cart, coupons)
-#   # Consult README for inputs and outputs
-#   #
-#   # This method should call
-#   # * consolidate_cart
-#   # * apply_coupons
-#   # * apply_clearance
-#   #
-#   # BEFORE it begins the work of calculating the total (or else you might have
-#   # some irritated customers
-# end
+def checkout(cart, coupons)
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
+end
