daa8ff663d498ce875def8557b6085356c13d4ff
Tommy Berman <tomy.berman1@gmail.com>
2019-12-05 14:38:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,19 @@
+require 'pry'
+
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  items_index = 0
+  while items_index < collection.length
+    matched_item = collection[items_index]
+      if name === matched_item[:item]
+        return matched_item
+        else nil
+      end
+    items_index += 1
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,7 +21,26 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  unique_items = []
+  cart_index = 0
+  while cart_index < cart.length do
+      item_name = cart[cart_index][:item]
+      item_stats = cart[cart_index[:count]
+      
+    if unique_items[item_name]
+      item_stats += 1
+      else
+      unique_items[item_stats] = 1
+    end
+  cart_index += 1
+    end
+  unique_items
 end
+binding.pry
+  
+  
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
